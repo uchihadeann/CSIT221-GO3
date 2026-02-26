db689c02e9ac9401eb07b3cb0c9eb2eef8aab45c
deanugh <uchihadeann@gmail.com>
2026-02-26 22:19:17 +0800

    leetcode 2

--- Main.cpp
+++ Main.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
-
-        for (int i = 0; i < numRows; i++) {
-            vector<int> row(i + 1, 1);
-
-            for (int j = 1; j < i; j++) {
-                row[j] = result[i - 1][j - 1] + result[i - 1][j];
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < nums.size(); j++){
+                if(nums[i]+nums[j] == target && i != j){
+                    return {i, j};
+                }
             }
-
-            result.push_back(row);
         }
-
-        return result;
+        return {};
     }
 };
