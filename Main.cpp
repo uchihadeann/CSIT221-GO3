99a69634f38e59438236a224becd12cda9133bdf
deanugh <uchihadeann@gmail.com>
2026-03-13 19:06:08 +0800

    dsa leetcode act 3
    Removed twoSum function and added isPalindrome function.

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < nums.size(); j++){
-                if(nums[i]+nums[j] == target && i != j){
-                    return {i, j};
-                }
-            }
-        }
-        return {};
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        string r = s;
+        reverse(r.begin(), r.end());
+        return s == r;
     }
 };
