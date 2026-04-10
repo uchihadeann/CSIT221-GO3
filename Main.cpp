42a2c68b920485dc41feb5209b77024ad464481b
deanugh <uchihadeann@gmail.com>
2026-04-10 21:56:48 +0800

    Add removeElement function to remove specified valueleetcode

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        string s = to_string(x);
-        string r = s;
-        reverse(r.begin(), r.end());
-        return s == r;
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
     }
 };
